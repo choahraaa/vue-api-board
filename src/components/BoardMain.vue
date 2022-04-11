166fd736eff63f9371138c611242bcda240f4de8
ahracho <ahra123@i-commtech.com>
2022-04-11 14:23:15 +0900

    기본 틀 형성

---
+++ src/components/BoardMain.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>게시판 등록</h2>
+    <table>
+      <tr>
+        <th>제목 :</th>
+        <input type="text" placeholder="제목을 입력해주세요.">
+      </tr>
+      <tr>
+        <th>본문 :</th>
+        <textarea placeholder="본문을 입력해주세요."></textarea>
+      </tr>
+    </table>
+    <button>저장</button>
+    <button>취소</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "boardMain"
+}
+</script>
+
+<style scoped>
+
+</style>
