5362bbdcd83253f429649a7206b3ce0705f4cebf
ahracho <ahra123@i-commtech.com>
2022-04-13 15:50:43 +0900

    axios 전역 설정 삭제

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import axios from "axios";
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-const app = createApp(App).mount('#app')
-app.config.globalProperties.axios = axios;
+createApp(App).mount('#app')
