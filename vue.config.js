5d0b710afa98ab3bd74a60e935a96530d9a6564c
ahracho <ahra123@i-commtech.com>
2022-04-14 14:55:40 +0900

    proxy 설정 add

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api": { //  /api 의 경로 밑에 있는 것들을 다 포함
+        target: 'http://localhost:9090',
+        changeOrigin: true,
+      },
+    },
+  }
 })
