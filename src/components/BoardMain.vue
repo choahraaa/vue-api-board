6e661e12596d9c0867d3b2bd85435b7f295a7267
ahracho <ahra123@i-commtech.com>
2022-04-15 15:02:15 +0900

    게시글 목록 add

---
+++ src/components/BoardMain.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>게시글 목록</h2>
+    <table>
+      <tr>
+        <th>제목</th>
+        <th>본문</th>
+      </tr>
+      <tr v-for="board in boardList" :key="board">
+        <td>{{board.title}}</td>
+        <td>{{board.content}}</td>
+      </tr>
+    </table>
+    <button>글쓰기</button>
+    <button @click="search">조회</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "BoardMain",
+  data() {
+    return {
+      boardList: []
+    }
+  },
+  methods: {
+    search() {
+      axios.get('/api/api-board')
+      .then((response) => {
+        this.boardList = response.data
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
