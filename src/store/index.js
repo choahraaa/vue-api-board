732324808b151b86dacc198b73374ca19e3a8a1e
ahracho <ahra123@i-commtech.com>
2022-04-18 16:16:47 +0900

    fetch add

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,30 @@ import axios from "axios";
 
 export default createStore({
   state: {
-    boardList: []
+    boardList: [],
+    board: {}
   },
   getters: {
   },
   mutations: {
-    set_boardList(state, boardList) {
+    setBoardList(state, boardList) {
       state.boardList = boardList
+    },
+    setBoard(state, board) {
+      state.board = board
     }
   },
   actions: {
     search({commit}) {
       axios.get('/api/api-board')
           .then((response) => {
-            commit('set_boardList', response.data);
+            commit('setBoardList', response.data);
+          })
+    },
+    fetch({commit}, id) {
+      axios.get(`/api/api-board/${id}`)
+          .then((response) => {
+            commit('setBoard', response.data);
           })
     }
   },
