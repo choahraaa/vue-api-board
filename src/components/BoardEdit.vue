aef001af2f6a7f9d9a9e4e189b8eccb3cb530892
ahracho <ahra123@i-commtech.com>
2022-04-18 13:42:31 +0900

    글 수정 vue add

---
+++ src/components/BoardEdit.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <h2>게시판 수정</h2>
+  <table>
+    <tr>
+      <th>제목 :</th>
+      <td>
+        <input type="text" placeholder="제목을 입력해주세요.">
+      </td>
+    </tr>
+    <tr>
+      <th>본문 :</th>
+      <td>
+        <textarea placeholder="본문을 입력해주세요."></textarea>
+      </td>
+    </tr>
+  </table>
+  <button>수정</button>
+  <button>취소</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "BoardEdit",
+  methods: {
+
+  }
+}
+</script>
