72ddd826dd53f116cf2348f7539063940a0ec6fb
ahracho <ahra123@i-commtech.com>
2022-04-28 15:05:59 +0900

    store state 함수로 수정

--- src/store/index.js
+++ src/store/index.js
@@ -1,70 +1,70 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 import axios from "axios";
 import router from "../router";
 
 export default createStore({
-  state: {
-    boardList: [],
-    board: {
-      title: '',
-      content:''
-    }
-  },
-  getters: {
-  },
-  mutations: {
-    setBoardList(state, boardList) {
-      state.boardList = boardList
+    state() {
+        return {
+            boardList: [],
+            board: {
+                title: '',
+                content: ''
+            }
+        }
     },
-    setBoard(state, board) {
-      state.board = board
+    getters: {},
+    mutations: {
+        setBoardList(state, boardList) {
+            state.boardList = boardList
+        },
+        setBoard(state, board) {
+            state.board = board
+        },
     },
-  },
-  actions: {
-    search({commit}) {
-      axios.get('/api/api-board')
-          .then((response) => {
-            commit('setBoardList', response.data);
-          })
-    },
-    fetch({commit}, id) {
-      axios.get(`/api/api-board/${id}`)
-          .then((response) => {
-            commit('setBoard', response.data);
-          })
-    },
-    update(context, id) {
-      axios.put(`/api/api-board/${id}`
-          , {
-            title : this.state.board.title,
-            content : this.state.board.content
-          })
-          .then(() => {
-            alert('수정되었습니다.');
-          })
-    },
-      insert() {
-          axios.post('/api/api-board', {
-              title: this.state.board.title,
-              content: this.state.board.content
-          })
-              .then(() => {
-                  alert('저장되었습니다.');
-                  this.state.board.title = '';
-                  this.state.board.content = '';
-                  router.push('/');
-              });
-      },
+    actions: {
+        search({commit}) {
+            axios.get('/api/api-board')
+                .then((response) => {
+                    commit('setBoardList', response.data);
+                })
+        },
+        fetch({commit}, id) {
+            axios.get(`/api/api-board/${id}`)
+                .then((response) => {
+                    commit('setBoard', response.data);
+                })
+        },
+        update(context, id) {
+            axios.put(`/api/api-board/${id}`
+                , {
+                    title: this.state.board.title,
+                    content: this.state.board.content
+                })
+                .then(() => {
+                    alert('수정되었습니다.');
+                })
+        },
+        insert() {
+            axios.post('/api/api-board', {
+                title: this.state.board.title,
+                content: this.state.board.content
+            })
+                .then(() => {
+                    alert('저장되었습니다.');
+                    this.state.board.title = '';
+                    this.state.board.content = '';
+                    router.push('/');
+                });
+        },
 
-      delete(context, id) {
-        axios.delete(`/api/api-board/${id}`)
-            .then(() => {
-                alert('삭제되었습니다.');
-                router.push('/');
-        });
+        delete(context, id) {
+            axios.delete(`/api/api-board/${id}`)
+                .then(() => {
+                    alert('삭제되었습니다.');
+                    router.push('/');
+                });
 
-      }
-  },
-  modules: {
-  }
+        }
+    },
+    modules: {}
 })
