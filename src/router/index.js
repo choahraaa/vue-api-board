e915ad0da839a76b8d29a2bcf1b263987604c834
ahracho <ahra123@i-commtech.com>
2022-05-09 11:45:07 +0900

    createMember router add

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import BoardInsert from "@/components/BoardInsert";
 import BoardMain from "@/components/BoardMain";
 import BoardEdit from "@/components/BoardEdit";
 import LoginPage from "@/components/LoginPage";
+import CreateMember from "@/components/CreateMember";
 
 const routes = [
     {
         path: '/',
         name: 'LoginPage',
         component: LoginPage
+    },
+    {
+        path: '/createMember',
+        name: 'CreateMember',
+        component: CreateMember
     },
     {
         path:'/main',
