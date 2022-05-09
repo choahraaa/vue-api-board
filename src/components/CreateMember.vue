50a47184bf344f7d2d5d19b0e30224320f6baa5f
ahracho <ahra123@i-commtech.com>
2022-05-09 11:42:45 +0900

    회원가입 component add

---
+++ src/components/CreateMember.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <table>
+    <tr>
+      <th>이름 :</th>
+      <td>
+        <input type="text" v-model="this.$store.state.board.title" placeholder="이름을 입력해주세요.">
+      </td>
+    </tr>
+    <tr>
+      <th>아이디 :</th>
+      <td>
+        <input type="text" v-model="this.$store.state.board.content" placeholder="아이디를 입력해주세요.">
+      </td>
+    </tr>
+    <tr>
+      <th>비밀번호 :</th>
+      <td>
+        <input type="text" v-model="this.$store.state.board.content" placeholder="비밀번호를 입력해주세요.">
+      </td>
+    </tr>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+  name: "CreateMember"
+}
+</script>
+
+<style scoped>
+
+</style>
