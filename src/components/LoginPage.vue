0a7e561059503203727135086deb28eac0a4bb2a
ahracho <ahra123@i-commtech.com>
2022-05-10 22:51:01 +0900

    login v-model data 처리 add

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,30 +1,45 @@
 <template>
-<div>
-  <table>
-    <tr>
-      <th>아이디 :</th>
-      <td>
-        <input type="text">
-      </td>
-    </tr>
-    <tr>
-      <th>비밀번호 :</th>
-      <td>
-        <input type="password">
-      </td>
-    </tr>
-    <button>로그인</button>
+  <div>
+    <table>
+      <tr>
+        <th>아이디 :</th>
+        <td>
+          <input type="text" v-model="loginIdDD">
+        </td>
+      </tr>
+      <tr>
+        <th>비밀번호 :</th>
+        <td>
+          <input type="password" v-model="this.$store.state.login.user.loginPw">
+        </td>
+      </tr>
+    </table>
+    <button @click="loginUser">로그인</button>
     <button @click="createMember">회원가입</button>
-  </table>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "LoginPage",
+  computed: {
+    loginIdDD: {
+      get() {
+        return this.$store.state.login.user.loginId;
+      }, set(value) {
+        this.$store.commit("login/setLoginId", value);
+      }
+    }
+  },
+  created() {
+    console.log('state', this.$store)
+  },
   methods: {
     createMember() {
       this.$router.push('/createMember')
+    },
+    loginUser() {
+      this.$store.dispatch('login/login')
     }
   }
 }
